2eab9d03e74ba11d6f31158f0ba4830b0bb996fc
Garrett <grolemund@gmail.com>
2011-05-05 14:56:44 -0500

    debugged map-zoomer.r

--- map-zoomer.r
+++ map-zoomer.r
@@ -1,18 +1,20 @@
 # source("~/Documents/cranvas/demos/map-zoomer.r", chdir= T)
 library(qtpaint)
+library(qtbase) # Need for qrect and others
 library(ggplot2)
 
 if (!exists("geo")) {
   load("geo.rdata")  
 }
+
 df <- data.frame(y = geo$lat * 100, x = geo$long * 100)
-df <- df[complete.cases(df), ]
+df <- df[complete.cases(df), ] 
 
 scatterplot <- function(layer, painter, exposed) {
-  qstrokeColor(painter) <- "black"
+  qstrokeColor(painter) <- "black" # note: faster to remove stroke
   qfillColor(painter) <- "black"
-  if (zoom_level > 10) {
-    circle <- qpathCircle(0, 0, 2)    
+  if (zoom_level > 10) { 
+    circle <- qglyphCircle(2) # couldn't find qpathCircle
     qdrawGlyph(painter, circle, df$x, df$y)    
   } else {
     qdrawPoint(painter, df$x, df$y)    
@@ -26,47 +28,54 @@ pos <- c(midmean(df$x), midmean(df$y))
 rng <- c(half_range(df$x), half_range(df$y))
 zoom_level <- 0
 
-mouse_zoom <- function(event) {
-  pos <<- event$pos
-  if (event$modifiers[["control"]]) {
+
+mouse_zoom <- function(level, event) { # added level
+  # have to use environment type subsetting with event$pos
+  pos <<- c(event$pos()$x(), event$pos()$y())
+    if (event$modifiers() == 268435456) { # again the output is integers
     zoom_out()
   } else {
     zoom_in()
   }
+
 }
 
-zoom_in <- function(event, ...) {  
+zoom_in <- function(event, ...) {
   zoom_level <<- zoom_level + 1
   zoom_update()
 }
+
 zoom_out <- function() {
   zoom_level <<- zoom_level - 1
   zoom_update()  
 }
+
 zoom_update <- function() {
-  qlimits(points) <- qrect(
+  # couldn't find qlimit(), so accessed points$setLimits
+  points$setLimits(qrect(
     c(pos[1] - rng[1] / 1.4 ^ zoom_level, pos[1] + rng[1] / 1.4 ^ zoom_level), 
     c(pos[2] - rng[2] / 1.4 ^ zoom_level, pos[2] + rng[2] / 1.4 ^ zoom_level)
-  )
+  ))
 }
 
-handle_keys <- function(event) {
-  if (event$key == "up") {
+
+handle_keys <- function(level, event) { # added level
+  if (event$key() == 16777235) { # changed to the function and integer output
     pos[2] <<- pos[2] + rng[2] / zoom_level / 8
-  } else if (event$key == "down") {
+  } else if (event$key() == 16777237) { # and here
     pos[2] <<- pos[2] - rng[2] / zoom_level / 8
-  } else if (event$key == "left") {
+  } else if (event$key() == 16777234) { # and here
     pos[1] <<- pos[1] - rng[1] / zoom_level / 8
-  } else if (event$key == "right") {
+  } else if (event$key() == 16777236) { # and here
     pos[1] <<- pos[1] + rng[1] / zoom_level / 8
   }
   zoom_update()
   qupdate(scene)
 }  
 
-scene <- qgraphicsScene()
+scene <- Qt$QGraphicsScene() # have to call QGraphicsScene from the Qt object
 points <- qlayer(scene, scatterplot, 
-  mouseDoubleClickFun = mouse_zoom, keyPressFun = handle_keys)
+  mouseDoubleClickFun = mouse_zoom, keyPressFun = handle_keys) 
 zoom_update()
 
 view <- qplotView(scene = scene)
